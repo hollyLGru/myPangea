d99044fd7db912756053bff6962deae8412afd6f
HollyLgru <H.grudovich@zollege.com>
2022-09-07 12:02:19 -0500

    need to do HOME next

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,9 @@ import React from 'react'
 const Home = () => {
     return (
         <div className="card-container">
-           here is your pangea
+here is your pangea. this will have all of the journal entries. I also want to be able to sort them by continent.
+ you can click on an entry, read it, and then decide to delete or edit the entry. 
+
         </div>
     )
 }
