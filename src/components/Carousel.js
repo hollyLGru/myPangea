11389ec3fed4eddb1e95c0030f5a1ac5015a9cad
HollyLgru <H.grudovich@zollege.com>
2022-09-23 12:49:40 -0500

    done

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -19,10 +19,9 @@ function CarouselCard(props) {
           <img src="https://scontent.xx.fbcdn.net/v/t1.15752-9/305164238_532524808678311_6797750261834955110_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=aee45a&_nc_ohc=cYc0a564j3cAX_PoK8X&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=03_AVLwUqWYcRietegKTwXjz27l_6w1ZAIz2CehWmrtp49ooA&oe=634EB18F" />
           <div className="myCarousel">
             <h3>Tuesday, January 15th, 2020</h3>
-            <h4>Chiang Mai, Thailand</h4>
-            <h4>Asia</h4>
+            <h4>Chiang Mai, Thailand, Asia</h4>
             <p>
-              "Today we visited the elephant sanctuary in the jungles of Chiang Mai where we were greeted by five ...."
+              Today we visited the elephant sanctuary in the jungles of Chiang Mai where we were greeted by five ....
             </p>
           </div>
         </div>
@@ -31,10 +30,9 @@ function CarouselCard(props) {
           <img src="https://scontent-hou1-1.xx.fbcdn.net/v/t1.6435-9/87799459_1115454738796495_5639911604385480704_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=vjmnOC-QwhEAX-bXAlV&_nc_ht=scontent-hou1-1.xx&oh=00_AT937ui4ttSy3KVQUAIZ8rXqfgrotwJOQKD_P2NY4E_fmA&oe=63504C02" />
           <div className="myCarousel">
           <h3>Friday, March 5th, 2021</h3>
-            <h4>Cape Town, South Africa</h4>
-            <h4>Africa</h4>
+            <h4>Cape Town, South Africa, Africa</h4>
             <p>
-              "Not a cloud in the sky as we made our way towards the torquiose waters surrounding Cape Town penninsula..."
+              Not a cloud in the sky as we made our way towards the torquiose waters surrounding Cape Town penninsula...
             </p>
           </div>
         </div>
@@ -43,10 +41,9 @@ function CarouselCard(props) {
           <img src="https://scontent-hou1-1.xx.fbcdn.net/v/t1.18169-9/12963766_261603677514943_4359166766615038717_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=dF5m8k8sEFcAX_lCU0S&_nc_ht=scontent-hou1-1.xx&oh=00_AT-VMJIj2903TTPYgc3PJqSuk350cVEIdMJ1_oFhjYWvDw&oe=635016B0" />
           <div className="myCarousel">
             <h3>Saturday, April 10th, 2016</h3>
-            <h4>Amsterdam, Netherlands</h4>
-            <h4>Europe</h4>
+            <h4>Amsterdam, Netherlands, Europe</h4>
             <p>
-              "There is no smell sweeter than a field of freshly bloomed tulips nestled in the dutch countryside in April...."
+              There is no smell sweeter than a field of freshly bloomed tulips nestled in the dutch countryside...
             </p>
           </div>
         </div>
