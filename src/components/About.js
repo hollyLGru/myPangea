9b3ca3393fa1591285bce974081f72c45389ef55
HollyLgru <H.grudovich@zollege.com>
2022-10-14 10:10:30 -0500

    done!

--- src/components/About.js
+++ src/components/About.js
@@ -6,8 +6,19 @@ class About extends Component {
 
   render() {
     return (
-      <div className="text-gray">
-       write about myPangea
+      <div className="text-gray" 
+      style={{
+      margin: "auto",
+      textAlign: "center",
+      display: "block",
+      paddingTop: "5%",
+      paddingLeft: "5%"}}>
+        <div style={{position: "relative", paddingBottom: "56.25%"}}>
+            <iframe title="myFrame" src="https://www.loom.com/embed/6240e41228a9461686e55d67fef82af8" webkitallowfullscreen mozallowfullscreen allowfullscreen 
+              style={{position: "absolute", width: "80%", height: "80%"}}>
+            </iframe>
+          </div>
+
       </div>
     );
   }
